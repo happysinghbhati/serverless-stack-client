de7d22446e11f55d1d2ce3838bf5ca02fe977726
Happy Bhati <happybhati@Happys-MacBook-Pro.local>
2019-11-06 15:55:28 -0500

    Setting up the React(Connecting front end with backend aws Amplify)

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "US East (N. Virginia)",
+    BUCKET: "sample-notes-demo-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://ax6eny14z7.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_SHJ5bj8Uf",
+    APP_CLIENT_ID: "4sb1hpqo2oopu82of76a924u8u",
+    IDENTITY_POOL_ID: "us-east-1:8cb5cb73-8d23-4d70-8a86-40f02ec4ce1f"
+  }
+};
