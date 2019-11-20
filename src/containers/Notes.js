149f38614bf9ed3c2d26a95a8325d1d6bd6a02b2
Happy Bhati <happybhati@Happys-MacBook-Pro.local>
2019-11-19 21:14:05 -0500

    Delete Note

--- src/containers/Notes.js
+++ src/containers/Notes.js
@@ -86,6 +86,10 @@ async function handleSubmit(event) {
   }
 }
 
+async function deleteNote() {
+  return API.del("notes", `/notes/${props.match.params.id}`);
+}
+
 async function handleDelete(event) {
   event.preventDefault();
 
@@ -98,6 +102,14 @@ async function handleDelete(event) {
   }
 
   setIsDeleting(true);
+
+  try {
+    await deleteNote();
+    props.history.push("/");
+  } catch (e) {
+    alert(e);
+    setIsDeleting(false);
+  }
 }
 
 return (
